9fa20d01d12b639bfab64d5cfdadfab6c0dfb443
tiktokyi <tiktokyi@gmail.com>
2024-06-08 15:19:14 +0900

    Mod : SupplierEx, ConsumerEx 사용 예시

--- src/functionalInterface/ConsumerEx.java
+++ src/functionalInterface/ConsumerEx.java
@@ -1,5 +1,7 @@
 package functionalInterface;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Consumer;
 
 public class ConsumerEx {
@@ -7,5 +9,10 @@ public class ConsumerEx {
         // ex
         Consumer<String> consumer = (str) -> System.out.println(str.split(" ")[0]);
         consumer.andThen(System.out::println).accept("Hello World!");
+
+        // ex2
+        List<String> strings = Arrays.asList("apple", "banana", "cherry");
+        Consumer<String> printConsumer = System.out::println;
+        strings.forEach(printConsumer);
     }
 }
--- src/functionalInterface/SupplierEx.java
+++ src/functionalInterface/SupplierEx.java
@@ -4,8 +4,13 @@ import java.util.function.Supplier;
 
 public class SupplierEx {
     public static void main(String[] args) {
-        // ex
-        Supplier<String> supplier = () -> "Hello World!";
-        System.out.println(supplier.get());
+        // ex1
+        Supplier<String> stringSupplier = () -> "Hello World!";
+        System.out.println(stringSupplier.get());
+
+        // ex2
+        Supplier<Double> randomSupplier = Math::random;
+        System.out.println(randomSupplier.get());
+
     }
 }
